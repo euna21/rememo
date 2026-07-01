0778759621261478cb4136e63518ccf144a5c1ed
sumingazihater <soomin9587@naver.com>
2026-07-01 19:57:02 +0900

    firebase 충돌 해결

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -14,23 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-=======
-// src/firebase.ts
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
-
-// Firebase 초기화 (우리 프로젝트와 Firebase를 연결)
-const app = initializeApp(firebaseConfig);
-
-// 로그인/회원가입 기능을 담당할 auth 객체 뽑아내기
->>>>>>> da1fd286669e81d875cb0e4bd903159be6b26ae0
 export const auth = getAuth(app);
