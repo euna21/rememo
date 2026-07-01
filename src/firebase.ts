da1fd286669e81d875cb0e4bd903159be6b26ae0
euna21 <20242596@gmail.com>
2026-07-01 19:22:03 +0900

    firebase연동

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+// Firebase 초기화 (우리 프로젝트와 Firebase를 연결)
+const app = initializeApp(firebaseConfig);
+
+// 로그인/회원가입 기능을 담당할 auth 객체 뽑아내기
+export const auth = getAuth(app);
